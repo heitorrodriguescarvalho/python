e460943661b78ddb0390fa805690f6c957189e27
heitorrodriguescarvalho <heitorrodrigues2909@gmail.com>
2022-08-09 21:27:57 -0300

    Create ex101.py

---
+++ exercicios/ex101.py
@@ -0,0 +1,23 @@
+from datetime import date
+
+print('-=-' * 15, '\033[31mFunções para Sortear e Somar\033[m', '-=-' * 15)
+
+
+def voto(birthDate):
+    age = int((date.today() - birthDate).days / 365.2425)
+    if age < 16:
+        votar = 'Negado'
+    elif 15 < age > 18:
+        votar = 'Opcional'
+    elif age < 60:
+        votar = 'Obrigatório'
+    else:
+        votar = 'Opcional'
+    return age, votar
+
+
+birthDay = int(input('Dia do Nascimento: '))
+birthMonth = int(input('Mês do Nascimento: '))
+birthYear = int(input('Ano do Nascimento: '))
+data = voto(date(birthYear, birthMonth, birthDay))
+print(f'Uma pessoa de {data[0]} anos tem o voto \033[1m{data[1]}\033[0m.')

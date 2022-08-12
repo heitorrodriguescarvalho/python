9e686106dfa4ecf11475e7d90eb98f8d72411401
heitorrodriguescarvalho <heitorrodrigues2909@gmail.com>
2022-08-12 09:44:04 -0300

    Create ex107.py

---
+++ exercicios/ex107/moeda.py
@@ -0,0 +1,21 @@
+def aumentar(n, a):
+    aumento = n * (a / 100)
+    num = n + aumento
+    return num
+
+
+def diminuir(n, d):
+    diminui = n * (d / 100)
+    num = n - diminui
+    return num
+
+
+def metade(n):
+    num = n / 2
+    return num
+
+
+def dobro(n):
+    num = n * 2
+    return num
+

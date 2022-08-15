54cb1152770be0cddd835dbf906ae6dd44c14405
heitorrodriguescarvalho <heitorrodrigues2909@gmail.com>
2022-08-15 15:24:29 -0300

    Create ex110.py

---
+++ exercicios/ex110/teste.py
@@ -0,0 +1,13 @@
+import moeda
+
+print('-=-' * 15, '\033[31mFormatando Moedas em Python\033[m', '-=-' * 15)
+num = float(input('Digite um preço: R$'))
+aumento = int(input('Qual será o aumento calculado: '))
+while aumento < 0:
+    print('\033[31mValor Inválido\033[0m')
+    aumento = int(input('Qual será o aumento calculado: '))
+diminui = int(input('Qual será a diminuição calculada: '))
+while diminui < 0:
+    print('\033[31mValor Inválido\033[0m')
+    diminui = int(input('Qual será a diminuição calculada: '))
+moeda.resumo(num, aumento, diminui)
